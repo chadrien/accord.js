6c9e20d2158c97dd7c113a6c3d1f4c3ebc5a65de
Hadrien de Cuzey <hadrien.de.cuzey@futurice.com>
2017-09-15 09:26:52 +0300

    Better release script

--- contrib/publish.sh
+++ contrib/publish.sh
@@ -3,6 +3,14 @@
 # display commands and die on error
 set -xe
 
+if [ "$1" == "" ]; then
+  set +xe
+  >&2 echo "New version type was not given"
+  >&2 echo "See \`npm help version\` for available options"
+  exit 1
+fi
+versionType="$1" # major | minor | patch
+
 # make sure the working copy is clean
 if [ "$(git status --short | wc -l)" -ne "0" ]; then
   set +xe
@@ -18,16 +26,34 @@ git clean -dxf
 npm install
 npm test
 
+# build the docs
+npm run generate-docs
+newVersion=$(npm version --no-git-tag-version $versionType)
+
+# commit new docs and package version
+git add .
+git commit -m"Bump version $newVersion"
+
 # build the new dist
 npm run build
 
-# make sure something changed
-if [ "$(git status --short dist | wc -l)" -eq "0" ]; then
-  set +xe
-  >&2 echo "Nothing new to publish, the dist remained the same"
-  git checkout docs # the docs would have change because of a link to a specific commit hash that can safelly be ignored
-  exit 1
-fi
+# move to a new branch and only keep the dist and package.json
+git checkout --orphan $newVersion
+git reset HEAD --
+git add dist package.json
+git clean -dxf
 
+mv dist/* .
+rm -rf dist node_modules
+
+# commit and tag
 git add .
-git commit -m'Commit new build'
+git commit -m"Commit build for version $newVersion"
+git tag $newVersion
+
+# From here on, we only display helpful messages to the use, we don't need to see the commands anymore, they'll only be in the way
+set +x
+
+echo "Everything's done. You can check that things looks good and proceed with the release:"
+echo "git push origin --tags"
+echo "npm publish"
--- dist/utils/test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var assert_1 = require("assert");
-require("rxjs/add/observable/from");
-require("rxjs/add/operator/takeUntil");
-exports.assertEqual = assert_1.deepEqual;
